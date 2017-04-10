8defba299fc112f5b243f1160944cf17e7d15402
Cristian <>
2017-04-10 13:33:27 +0200

    Ejer80

---
+++ Ejer80/src/ejer80/Ejer80.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejer80;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Cristian
+ */
+public class Ejer80 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner teclado = new Scanner(System.in);
+    int num1;
+    System.out.println("Introduce el lado del cubo");
+    num1=teclado.nextInt();
+    area a1 = new area();
+    System.out.println("El area total del cubo es " + a1.area(num1) + " cm3");}
+    
+}
---
+++ Ejer80/src/ejer80/area.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejer80;
+
+/**
+ *
+ * @author Cristian
+ */
+public class area {
+    
+private int num1;
+private int num2;
+
+public int area(int num1){
+num2=(int) (Math.pow(num1,2)*6);
+return num2;}}
