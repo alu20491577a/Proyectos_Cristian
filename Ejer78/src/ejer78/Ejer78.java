9016876c17c69ffc6d62ed37d3b1694626a347d7
Cristian <>
2017-04-10 13:32:11 +0200

    Ejer78

---
+++ Ejer78/src/ejer78/Ejer78.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejer78;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Cristian
+ */
+public class Ejer78 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        Scanner teclado2 = new Scanner(System.in);
+    int num1;
+    int num2;
+    float num3;
+    System.out.println("Introduce la base del triangulo");
+    num1=teclado.nextInt();
+    System.out.println("Introduce la altura del triangulo");
+    num2=teclado2.nextInt();
+    area a1 = new area(num1,num2);
+    System.out.println("El area del triangulo es "+ a1.getnum3() + " cm2" );}
+    
+}
---
+++ Ejer78/src/ejer78/area.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejer78;
+
+/**
+ *
+ * @author Cristian
+ */
+public class area {
+    private int num1;
+    private int num2;
+    private float num3;
+    
+public area(int num1,int num2){
+this.num1=num1;
+this.num2=num2;
+num3=num1*num2/2;}
+
+public float getnum3(){
+return num3;}}
