e0e780f06aff7f9d794d87dd41516c0985c99220
Cristian <pablo@pablo-VirtualBox>
2017-04-25 13:10:02 +0200

    Ejer78

--- Ejer78/src/ejer78/area.java
+++ Ejer78/src/ejer78/area.java
@@ -15,12 +15,18 @@ public class area {
     private float num3;
     
     /** Mediante este constructor recibiremos los datos
-     * de la clase principal y conseguiremos el area del triangulo */
+     * de la clase principal y conseguiremos el area del triangulo
+     * @param num1 base del triangulo a calcular
+     * @param num2 altura del triangulo a calcular */
     
 public area(int num1,int num2){
 this.num1=num1;
 this.num2=num2;
 num3=num1*num2/2;}
 
-public float getnum3(){
+    /**
+     *
+     * @return devolvera el resultado del area
+     */
+    public float getnum3(){
 return num3;}}
