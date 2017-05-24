abe529fdd498305abe648f4f58744a8edf365d83
Cristian <pablo@pablo-VirtualBox>
2017-05-24 08:57:59 +0200

    Ejer86(Transformación de distancias)

--- Ejer86/src/ejer86/Ejer86.java
+++ Ejer86/src/ejer86/Ejer86.java
@@ -14,7 +14,7 @@ public class Ejer86 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
         // TODO code application logic here
     
     String opcion;
