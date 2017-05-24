47779e5da5356cd7d27cd98f833be6c50190b3ec
Cristian <pablo@pablo-VirtualBox>
2017-05-24 08:55:42 +0200

    Ejer83(Ecuacion de segundo grado)

--- Ejer83/src/ejer83/Ejer83.java
+++ Ejer83/src/ejer83/Ejer83.java
@@ -14,7 +14,7 @@ public class Ejer83 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
         // TODO code application logic here
     int x2;
     int x;
