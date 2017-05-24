b33060c523eaf793c1ed16cb61c38432b3c5d712
Cristian <pablo@pablo-VirtualBox>
2017-05-24 09:00:24 +0200

    Ejer91(Comprobar fecha)

--- Ejer91/src/ejer91/Ejer91.java
+++ Ejer91/src/ejer91/Ejer91.java
@@ -14,7 +14,7 @@ public class Ejer91 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
         // TODO code application logic here);
     Menu m1 = new Menu();
     m1.Menu();}
