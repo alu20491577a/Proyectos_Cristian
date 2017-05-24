818ca4e5d6c8689d0ed41d810be891de46c45e63
Cristian <pablo@pablo-VirtualBox>
2017-05-24 08:54:08 +0200

    Ejer81(Esfera)

--- Ejer81/src/ejer81/Ejer81.java
+++ Ejer81/src/ejer81/Ejer81.java
@@ -16,7 +16,7 @@ public class Ejer81 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
         // TODO code application logic here
     
         Scanner teclado = new Scanner(System.in);
