608f90327ace19682388ca13db93e4ecc6295e16
Cristian <pablo@pablo-VirtualBox>
2017-05-24 08:59:19 +0200

    Ejer89(Contador)

--- Ejer89/src/ejer89/Ejer89.java
+++ Ejer89/src/ejer89/Ejer89.java
@@ -14,7 +14,7 @@ public class Ejer89 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
         // TODO code application logic here
     Contador c1 =  new Contador();
     Menu m1 = new Menu();
